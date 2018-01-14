bd537e3be0d0af4436507012a75a99b6cca7ee4e
Spencer Ang <boomerandzapper@gmail.com>
2018-01-13 18:41:47 -0800

    Reddit information from json done.

--- reddit_api_script.py
+++ reddit_api_script.py
@@ -1,5 +1,35 @@
 import urllib, json
+unloaded = True
+numberofposts = 5
 url = "https://www.reddit.com/r/aww.json"
-response = urllib.urlopen(url)
-data = json.loads(response.read())
-print data['data']['children'][0]
+while unloaded:
+
+  response = urllib.urlopen(url)
+  data = json.loads(response.read())
+  if 'data' in data:
+    unloaded = False
+    sum = 0;
+    index = 0;
+
+
+    while sum < numberofposts:
+
+
+      if not data['data']['children'][index]['data']['stickied']:
+
+        #title
+        print data['data']['children'][index]['data']['title']
+
+        #image if there is one, or body
+        if data['data']['children'][index]['data']['selftext_html'] is None:
+          #image
+          print data['data']['children'][index]['data']['preview']['images'][0]['source']['url']
+        else:
+          #body
+          print data['data']['children'][index]['data']['selftext']
+
+
+        sum = sum + 1
+      index = index + 1
+  else:
+    print "Loading..."
